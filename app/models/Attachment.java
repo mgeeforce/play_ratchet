cbf6e810363bf125659ba0f56e664183a47e55ad
Mike Gee <mgeeforce@gmail.com>
2014-06-01 15:48:03 -0600

    not committing or pushing frequently enough

--- app/models/Attachment.java
+++ app/models/Attachment.java
@@ -5,13 +5,14 @@ package models;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
 import javax.persistence.Id;
 
 import com.avaje.ebean.annotation.CreatedTimestamp;
 
 import play.db.ebean.Model;
 
-
+@Entity
 public class Attachment extends Model {
 	
     private static final long serialVersionUID = 1L;
